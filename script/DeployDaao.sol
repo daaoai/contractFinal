c72358921d431c85cd1ae46eeabb69a6752933b9
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-01-27 14:59:32 +0530

    fix::> Updated imports
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- script/DeployDaao.sol
+++ script/DeployDaao.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.18;
 import {console2} from "forge-std/console2.sol";
 
 import {Script} from "forge-std/Script.sol";
-import {DaosWorldV1Token} from "../src/DaosWorldV1Token.sol"; // Adjust the path based on your project structure
-import {DaosWorldV1} from "../src/DaosWorldV1.sol"; // Adjust the path based on your project structure
+import {DaaoToken} from "../src/DaaoToken.sol"; // Adjust the path based on your project structure
+import {Daao} from "../src/Daao.sol"; // Adjust the path based on your project structure
 
 contract DeployDaosWorld is Script {
     function run() public {
@@ -13,31 +13,29 @@ contract DeployDaosWorld is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy DaosWorldV1Token
-        DaosWorldV1Token token = new DaosWorldV1Token("tokenTwo", "T2");
-     
-        console2.log("DaosWorldV1Token deployed at:", address(token));
+        // Deploy DaaoToken
+        DaaoToken token = new DaaoToken("tokenTwo", "T2");
 
-        // Deploy DaosWorldV1
+        console2.log("DaaoToken deployed at:", address(token));
 
+        // Deploy Daao
 
-        uint256 fundraisingGoal = 0.00002 ether; // 100 
+        uint256 fundraisingGoal = 0.00002 ether; // 100
         uint256 maxWhitelistAmount = 0.1 ether; // Max 10 ETH per whitelist
         uint256 maxPublicContributionAmount = 20 ether; // Max 20 ETH for public contribution
-       
-
 
         uint256 fundraisingDeadline = block.timestamp + 7 days; // 7 days from now
-      
- uint256 fundExpiry = fundraisingDeadline + 120; // 30 days after deadline
-        
+
+        uint256 fundExpiry = fundraisingDeadline + 120; // 30 days after deadline
+
         address daoManager = vm.addr(deployerPrivateKey); // Address derived from the deployer private key
-        address liquidityLockerFactory = address(0xaEDEDdDC448AEE5237f6b3f11Ec370aB5793A0d3); // Replace with actual address
-     
+        address liquidityLockerFactory = address(
+            0xaEDEDdDC448AEE5237f6b3f11Ec370aB5793A0d3
+        ); // Replace with actual address
+
         address protocolAdmin = daoManager; // Protocol admin same as DAO manager
 
-
-        // DaosWorldV1 daosWorldV1 = new DaosWorldV1(
+        // Daao daosWorldV1 = new Daao(
         //     fundraisingGoal,
         //     "DAO Token",
         //     "DAO",
@@ -49,9 +47,10 @@ contract DeployDaosWorld is Script {
         //     protocolAdmin,
         //     maxPublicContributionAmount
         // );
-        token.transferOwnership(address(0x4B4f3C9126197fcCF0d92fBA0C194F0DffF16072));
-     
-        console2.log("Daos manager is ",daoManager);
-       
+        token.transferOwnership(
+            address(0x4B4f3C9126197fcCF0d92fBA0C194F0DffF16072)
+        );
+
+        console2.log("Daos manager is ", daoManager);
     }
 }
--- script/FinalizeFundraising.s.sol
+++ script/FinalizeFundraising.s.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.18;
 
 import {console2} from "forge-std/console2.sol";
 import {Script} from "forge-std/Script.sol";
-import {DaosWorldV1} from "../src/DaosWorldV1.sol";
-import {DaosWorldV1Token} from "../src/DaosWorldV1Token.sol";
+import {Daao} from "../src/Daao.sol";
+import {DaaoToken} from "../src/DaaoToken.sol";
 
 contract FinalizeFundraising is Script {
     function run() public {
@@ -15,14 +15,14 @@ contract FinalizeFundraising is Script {
         address payable daosWorldV1Address = payable(
             vm.envAddress("DAOS_WORLD_V1_ADDRESS")
         );
-        DaosWorldV1 daosWorldV1 = DaosWorldV1(daosWorldV1Address);
+        Daao daosWorldV1 = Daao(daosWorldV1Address);
 
         address daosWorldV1TokenAddress = vm.envAddress(
             "DAOS_WORLD_V1_TOKEN_ADDRESS"
         );
 
-        int24 initialTick = 6800; 
-        int24 upperTick = 7200;   
+        int24 initialTick = 6800;
+        int24 upperTick = 7200;
 
         daosWorldV1.finalizeFundraising(initialTick, upperTick);
         console2.log("Fundraising finalized for contract:", daosWorldV1Address);
